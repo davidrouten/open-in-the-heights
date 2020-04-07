d80fe2f4f7512fc5c4fb9f54494d5fb04e96d5aa
David R <davidellisr@yahoo.com>
2020-04-07 16:58:23 -0400

    Remove redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  PREFERRED_HOST = ENV.fetch('PREFERRED_HOST', 'www.openintheheights.com')
-  before_filter :redirect_to_prefered_host
-
-  private
-
-  def redirect_to_prefered_host
-    if Rails.env.production? && request.host != PREFERRED_HOST
-      redirect_to(host: PREFERRED_HOST)
-    end
-  end
 end
