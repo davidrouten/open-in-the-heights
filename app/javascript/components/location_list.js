756d766d287d94ba1da0cea4743943da58e15c56
David R <davidellisr@yahoo.com>
2020-04-07 20:38:46 -0400

    Add clearfix for mobile issue.

--- app/javascript/components/location_list.js
+++ app/javascript/components/location_list.js
@@ -139,7 +139,7 @@ export default class LocationList extends React.Component {
               <button
                 key={index}
                 onClick={() => this.props.setLocation(location['id'])}
-                className="list-group-item py-2 px-2 list-group-item-action"
+                className="list-group-item py-2 px-2 list-group-item-action clearfix"
               >
                 <div className="d-flex w-100 justify-content-between">
                   <h5 className="mb-1">{location['name']}</h5>
